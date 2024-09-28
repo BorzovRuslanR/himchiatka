e2aecb5c71b36894b8416cafef5a4a8f17542261
RuslanB <ruslan.borzov@in-zhir.ru>
2024-09-28 22:47:41 +0300

    Added icons item and prices block

--- script.js
+++ script.js
@@ -72,3 +72,43 @@ modal.onclick = event => {
     modal.style.display = "none";
   }
 };
+
+
+// ==================== UNIT PRICES ====================
+
+const icons = [
+  { name: 'Диван', price: '1000 руб', icon: '/icon/unit/sofa2.svg', description: '2 посадочных (100-120 см)' },
+  { name: 'Диван', price: '1500 руб', icon: '/icon/unit/sofa3.svg', description: '3 посадочных (150-180 см)' },
+  { name: 'Диван', price: '2500 руб', icon: '/icon/unit/sofa3cut.svg', description: '3 посадочных места + угол' },
+  { name: 'Диван', price: '2700 руб', icon: '/icon/unit/4sofa.svg', description: '4 посадочных места + угол' },
+  { name: 'Диван', price: '3500 руб', icon: '/icon/unit/p-sofa.svg', description: '6 посадочных (П-образный)' },
+  { name: 'Посадочное место', price: '500 руб', icon: '/icon/unit/sofa-seat.svg', description: '(50-60см)' },
+  { name: 'Спальное место', price: '1000 руб', icon: '/icon/unit/sleep-place.svg', description: 'Стандартное' },
+  { name: 'Посадочное место', price: '600 руб', icon: '/icon/unit/sofa-antivandal.svg', description: 'С антивандальной тканью' },
+  { name: 'Посадочное место', price: '1200 руб', icon: '/icon/unit/sofa-viscoza.svg', description: 'С обивкой из шелка, вискозы ...' },
+  { name: 'Кресла', price: '500 руб', icon: '/icon/unit/armchair.svg', description: '(50-60 см)' },
+  { name: 'Стулья', price: '200 руб', icon: '/icon/unit/chair.svg', description: 'Без мягкой спинки' },
+  { name: 'Стулья', price: '300 руб', icon: '/icon/unit/chair-soft.svg', description: 'С мягкой спинкой' },
+  { name: 'Кухонный уголок', price: '1300 руб', icon: '/icon/unit/kitchen.svg', description: 'Стандартный' },
+  { name: 'Изголовье кровати', price: '1500 руб', icon: '/icon/unit/bed.svg', description: 'Стандартное' },
+  { name: 'Выведение запахов', price: '500 руб', icon: '/icon/unit/aerosol.svg', description: 'Дополнительно' },
+  { name: 'Сильные загрязнения', price: '20 %', icon: '/icon/unit/mud.svg', description: 'Дополнительно' },
+];
+
+function generateIconsHTML(icons) {
+  const container = document.querySelector('.prices-unit');
+  icons.forEach(icon => {
+    const iconElement = document.createElement('div');
+    iconElement.classList.add('icon-item');
+    iconElement.innerHTML = `
+      <img src="${icon.icon}" alt="${icon.name}">
+      <p>${icon.name}</p>
+      <p>${icon.description}</p>
+      <p>${icon.price}</p>
+    `;
+    container.appendChild(iconElement);
+  });
+}
+
+// Вызов функции для генерации HTML
+generateIconsHTML(icons);
